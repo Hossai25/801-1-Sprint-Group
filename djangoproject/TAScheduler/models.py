cca6833191d25789468d413e7564384bf2e46cb8
Jon Olsen <jonolsen@uwm.edu>
2023-04-18 12:31:41 -0500

    Added in class skeleton for models.py
    Added fields for User, PublicInformation, and PrivateInformation

--- djangoproject/TAScheduler/models.py
+++ djangoproject/TAScheduler/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
+    account_type = models.CharField(max_length=25)
+
+
+class Course(models.Model):
+    pass
+
+
+class Lab(models.Model):
+    pass
+
+
+class PublicInformation(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    office_hours = models.CharField(max_length=25)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class PrivateInformation(models.Model):
+    address = models.CharField(max_length=25)
+    phone_number = models.CharField(max_length=25)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class CourseTA(models.Model):
+    pass
