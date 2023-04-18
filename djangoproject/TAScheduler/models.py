1b624b0db6af06d5e8d989f379944f47bade4992
MattSky8 <112981773+MattSky8@users.noreply.github.com>
2023-04-18 13:47:59 -0500

    Revert "Added in class skeleton for models.py"
    This reverts commit cca6833191d25789468d413e7564384bf2e46cb8.

--- djangoproject/TAScheduler/models.py
+++ djangoproject/TAScheduler/models.py
@@ -1,32 +1,3 @@
 from django.db import models
 
-
-class User(models.Model):
-    email = models.CharField(max_length=25)
-    password = models.CharField(max_length=25)
-    account_type = models.CharField(max_length=25)
-
-
-class Course(models.Model):
-    pass
-
-
-class Lab(models.Model):
-    pass
-
-
-class PublicInformation(models.Model):
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=25)
-    office_hours = models.CharField(max_length=25)
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-
-
-class PrivateInformation(models.Model):
-    address = models.CharField(max_length=25)
-    phone_number = models.CharField(max_length=25)
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-
-
-class CourseTA(models.Model):
-    pass
+# Create your models here.
