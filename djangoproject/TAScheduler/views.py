deb06006e3078bb49fec5dec3734bbd77d96f0e0
MattSky8 <112981773+MattSky8@users.noreply.github.com>
2023-04-19 21:44:06 -0500

    Update views.py

--- djangoproject/TAScheduler/views.py
+++ djangoproject/TAScheduler/views.py
@@ -1,75 +1,74 @@
 from django.shortcuts import render, redirect
 from django.views import View
 
-
 # Create your views here.
 class Accounts(View):
-    def get(self, request):
-        return render(request, "accounts.html", {})
+    def get(self,request):
+        return render(request,"accounts.html",{})
 
-    def post(self, request):
+    def post(self,request):
         pass
 
 class Course(View):
-    def get(self, request):
-        return render(request, "courses.html", {})
+    def get(self,request):
+        return render(request,"courses.html",{})
 
-    def post(self, request):
+    def post(self,request):
         pass
 
 class CreateAccount(View):
-    def get(self, request):
-        return render(request, "createAccount.html", {})
+    def get(self,request):
+        return render(request,"createAccount.html",{})
 
-    def post(self, request):
+    def post(self,request):
         pass
 
 class CreateCourse(View):
-    def get(self, request):
-        return render(request, "createCourse.html", {})
+    def get(self,request):
+        return render(request,"createCourse.html",{})
 
-    def post(self, request):
+    def post(self,request):
         pass
 
 class CreateLab(View):
-    def get(self, request):
-        return render(request, "createLab.html", {})
+    def get(self,request):
+        return render(request,"createLab.html",{})
 
-    def post(self, request):
+    def post(self,request):
         pass
 
 class Dashboard(View):
-    def get(self, request):
-        return render(request, "dashboard.html", {})
+    def get(self,request):
+        return render(request,"dashboard.html",{})
 
-    def post(self, request):
+    def post(self,request):
         pass
 
 class Database(View):
-    def get(self, request):
-        return render(request, "database.html", {})
+    def get(self,request):
+        return render(request,"database.html",{})
 
-    def post(self, request):
+    def post(self,request):
         pass
 
 class EditAccount(View):
-    def get(self, request):
-        return render(request, "editAccount.html", {})
+    def get(self,request):
+        return render(request,"editAccount.html",{})
 
-    def post(self, request):
+    def post(self,request):
         pass
 
 
 class LoginPage(View):
-    def get(self, request):
+    def get(self,request):
         """
         Get method for the LoginPage.
         :param request: An HttpRequest object from the loginPage template.
-        :return: A render of loginPage.html.
+        :return: A render of the request and loginPage.html.
         """
-        return render(request, "loginPage.html", {})
+        return render(request,"loginPage.html",{})
 
-    def post(self, request):
+    def post(self,request):
         """
         Post method for the LoginPage.
         :param request: An HttpRequest object from the loginPage template.
@@ -82,10 +81,9 @@ class LoginPage(View):
         """
         pass
 
-
 class Notifications(View):
-    def get(self, request):
-        return render(request, "notifications.html", {})
+    def get(self,request):
+        return render(request,"notifications.html",{})
 
-    def post(self, request):
+    def post(self,request):
         pass
