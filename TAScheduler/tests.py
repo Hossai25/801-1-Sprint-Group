435e28c49d62cfcb248961e09181eb02f59a7e1d
anna_fronk <fronkav@gmail.com>
2023-04-24 14:54:44 -0500

    More tests

--- TAScheduler/tests.py
+++ TAScheduler/tests.py
@@ -22,6 +22,8 @@ class Login(TestCase):
             temp3 = PrivateInfo(user_id=temp)
             temp3.save()
 
+
+    #This test checks to see if the username and password are passed from login into the database
     def test_correctName(self):
         for i in self.users:
             resp = self.webpage.post("/", {"username": i+"@uwm.edu", "password": i},
@@ -29,21 +31,25 @@ class Login(TestCase):
             self.assertEqual(resp.context["email"], i+"@uwm.edu", "username not passed from login")
 
 
+    #This test checks to see if upon a successful login the user is brought to the dashboard/homepage
     def test_successfulLogin(self):
         resp = self.webpage.post("/", {"username": "test1@uwm.edu", "password": "test1"},
                                  follow=True)
         self.assertRedirects(resp, "/dashboard/")
 
+    #This test checks to see that if no password is used an error message appears
     def test_noPassword(self):
         resp = self.webpage.post("/", {"username": "test1@uwm.edu", "password": ""},
                                  follow=True)
         self.assertContains(resp, "Invalid username or password.")
 
+    #This test checks to see if the wrong password is used an error message appears
     def test_wrongPassword(self):
         resp = self.webpage.post("/", {"username": "test1@uwm.edu", "password": "password"}, follow=True)
         self.assertContains(resp, "Invalid username or password.")
 
     def test_forgotPassword(self):
+        #HASN'T BEEN IMPLEMENTED YET
         pass
 
 class Dashboard(TestCase):
@@ -61,21 +67,40 @@ class Dashboard(TestCase):
             temp2.save()
             temp3 = PrivateInfo(user_id=temp)
             temp3.save()
+
+    #This test checks to see if the user is redirected to the accounts page when the button is pressed
     def test_accountsClicked(self):
         session = self.webpage.session
         session["email"] = "test1@uwm.edu"
         session.save()
         resp = self.webpage.get(reverse('dashboard'))
-        self.assertContains(resp, '<a class="btn btn-primary" href="%s">Create Accounts</a>' % reverse('createAccount'), html=True)
+        self.assertContains(resp, '<a class="btn btn-primary" href="%s">Accounts</a>' % reverse('accounts'), html=True)
 
+    # This test checks to see if the user is redirected to the courses page when the button is pressed
     def test_coursesClicked(self):
-        pass
+        session = self.webpage.session
+        session["email"] = "test1@uwm.edu"
+        session.save()
+        resp = self.webpage.get(reverse('dashboard'))
+        self.assertContains(resp, '<a class="btn btn-primary" href="%s">Courses/Labs</a>' % reverse('courses'), html=True)
 
+    # This test checks to see if the user is redirected to the access data page when the button is pressed
     def test_accessDataClicked(self):
-        pass
+        session = self.webpage.session
+        session["email"] = "test1@uwm.edu"
+        session.save()
+        resp = self.webpage.get(reverse('dashboard'))
+        self.assertContains(resp, '<a class="btn btn-primary" href="%s">Access Data</a>' % reverse('database'),
+                            html=True)
 
+    # This test checks to see if the user is redirected to the notifications page when the button is pressed
     def test_notificationsClicked(self):
-        pass
+        session = self.webpage.session
+        session["email"] = "test1@uwm.edu"
+        session.save()
+        resp = self.webpage.get(reverse('dashboard'))
+        self.assertContains(resp, '<a class="btn btn-primary" href="%s">Notifications</a>' % reverse('notifications'),
+                            html=True)
 
 class Accounts(TestCase):
     webpage = None
@@ -85,17 +110,29 @@ class Accounts(TestCase):
         self.webpage = Client()
         self.users = ["test1", "test2"]
 
+        # Fill test database with users
         for i in self.users:
             temp = User(email=i + "@uwm.edu", password=i, account_type="administrator")
             temp.save()
+            temp2 = PublicInfo(user_id=temp, first_name=i, last_name=i)
+            temp2.save()
+            temp3 = PrivateInfo(user_id=temp)
+            temp3.save()
 
     def test_toCreateAccountPage(self):
-        pass
+        session = self.webpage.session
+        session["email"] = "test1@uwm.edu"
+        session.save()
+        resp = self.webpage.get(reverse('accounts'))
+        self.assertContains(resp, '<a class="btn btn-primary" href="%s">Create Accounts</a>' % reverse('createAccount'),
+                            html=True)
 
     def test_toEditAccountPage(self):
+        #HASN"T BEEN IMPLEMENTED YET
         pass
 
     def test_toHomepage(self):
+        #HASN"T BEEN IMPLEMENTED YET
         pass
 
 
@@ -107,6 +144,43 @@ class CreateAccounts(TestCase):
         self.webpage = Client()
         self.users = ["test1", "test2"]
 
+        # Fill test database with users
         for i in self.users:
             temp = User(email=i + "@uwm.edu", password=i, account_type="administrator")
             temp.save()
+            temp2 = PublicInfo(user_id=temp, first_name=i, last_name=i)
+            temp2.save()
+            temp3 = PrivateInfo(user_id=temp)
+            temp3.save()
+
+    def test_checkFirstNameSuccessfully(self):
+        resp = self.webpage.post("/createAccount/", {"first_name": "Anna", "last_name": "Fronk", "email":
+            "avfronk@uwm.edu", "password": "annafronk", "account_type": "administrator"}, follow=True)
+        self.assertRedirects(resp, "/dashboard/")
+
+    def test_checkFirsNameFail(self):
+        pass
+
+    def test_checkLastNameSuccessfully(self):
+        pass
+
+    def test_checkLastNameFail(self):
+        pass
+
+    def test_checkEmailSuccessfully(self):
+        pass
+
+    def test_checkEmailFail(self):
+        pass
+
+    def test_checkPasswordSuccessfully(self):
+        pass
+
+    def test_checkPasswordFail(self):
+        pass
+
+    def test_checkAccountTypeSuccessfully(self):
+        pass
+
+    def test_checkAccountTypeFail(self):
+        pass
