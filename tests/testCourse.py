0bce1177ec42e326869e5cdc605ab04a04a227cc
anna_fronk <fronkav@gmail.com>
2023-04-25 19:10:11 -0500

    Unit tests for course started

--- tests/testCourse.py
+++ tests/testCourse.py
@@ -1,9 +1,40 @@
 import unittest
+from django.test import TestCase
+
+from TAScheduler.models import Course as CourseModel
+from classes import course
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestCourseMethods(TestCase):
+    def test_createcourse(self):
+        course_object = course.create_course("new name")
+        self.assertIsInstance(course_object, course.Course)
+
+    def test_createduplicatecourse(self):
+        course_object1 = course.create_course("new name")
+        course_object2 = course.create_course("new name")
+        self.assertEqual(course_object2, None)
+
+    def test_getCourseModelSuccessfully(self):
+        pass
+
+    def test_getUnmadeCourseModel(self):
+        pass
+
+    def test_getCourseSuccessfully(self):
+        pass
+
+    def test_getUnmadeCourse(self):
+        pass
+
+    def test_getCourseIDSuccessfully(self):
+        pass
+
+    def test_getUndeclaredClassID(self):
+        pass
+
+    def test_getEmptyCourseList(self):
+        pass
 
 
 if __name__ == '__main__':
--- tests/testSection.py
+++ tests/testSection.py
@@ -1,9 +1,14 @@
 import unittest
+from TAScheduler.models import Lab
+from classes.section import Section, create_section
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    #def test_something(self):
+        #self.assertEqual(True, False)  # add assertion here
+
+    def test_createSectionSuccesful(self):
+        self.assertEqual(create_section("testsection"), Lab(lab_name="testsection"))
 
 
 if __name__ == '__main__':
