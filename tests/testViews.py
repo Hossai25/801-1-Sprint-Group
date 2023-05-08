a346376e1a1000a99d760d7e99c0737afe0c6a20
MattSky8 <112981773+MattSky8@users.noreply.github.com>
2023-05-07 19:59:46 -0500

    Update testViews.py

--- tests/testViews.py
+++ tests/testViews.py
@@ -1,13 +1,18 @@
-from django.test import TestCase
+from django import test
 from TAScheduler import views
 
-class TestDisplayCourseGetContext(TestCase):
+class TestDisplayCourseGetContext(test.TestCase):
     def setUp(self):
-        pass
+        self.view = views.DisplayCourse()
+        self.request = {"email": "test_email", "account_type": "test_account_type"}
 
     def test_method_exists(self):
+        with self.assertRaises(TypeError):
+            self.view.get_context()
 
+    def test_x(self):
+        pass
 
 
 if __name__ == '__main__':
-    unittest.main()
+    test.unittest.main()
