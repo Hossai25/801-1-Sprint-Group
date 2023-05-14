9d088508c437a39889eca513b252bb93940c84ff
Marvin <marvhossain@gmail.com>
2023-05-13 19:09:39 -0500

    Fixed minor issue, now instructors can only edit section TAs for their OWN courses

--- TAScheduler/views.py
+++ TAScheduler/views.py
@@ -278,6 +278,7 @@ class DisplayCourse(View):
     def get_context(self, request, course_id):
         # TODO: unit tests
         course_obj = course.get_course_by_id(course_id)
+        current_user = account.get_account(request.session["email"])
         ta_list = ta.get_all_tas()
         instructor_list = instructor.get_all_instructors()
         course_tas = ta.get_course_tas(course_id)
@@ -294,6 +295,7 @@ class DisplayCourse(View):
         context = {"email": request.session["email"],
                    "account_type": request.session["account_type"],
                    "user": request.session["user"],
+                   'current_user': current_user,
                    'course': course_obj,
                    'course_tas': course_tas,
                    'course_instructor': course_instructor,
