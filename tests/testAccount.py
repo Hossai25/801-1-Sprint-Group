4332f2edea11afa9aa5ece23f285d26c65e05dd6
aataullah <ataullah@uwm.edu>
2023-05-16 18:28:55 -0500

    completed tests in testAccount.py

--- tests/testAccount.py
+++ tests/testAccount.py
@@ -69,53 +69,19 @@ class TestCreateAccount(TestCase):
         result = account.create_account(data2)
         self.assertEqual(result, None)
 
-
-#<<<<<<< HEAD
-    def test_has_required_fields(self):
-    # Test case 1: All required fields are present
-        data1 = {
-            'email': 'test@example.com',
-            'first_name': 'John',
-            'last_name': 'Doe',
-            'password': 'password123',
-            'user_type': 'admin',
-            'account_type': 'premium'
-        }
-        self.assertTrue(has_required_fields(data1))
-
-    # Test case 2: Missing required fields
-        data2 = {
-            'email': 'test@example.com',
-            'first_name': 'John',
-            'last_name': 'Doe',
-            'user_type': 'admin',
-            'account_type': 'premium'
-        }
-        self.assertFalse(has_required_fields(data2))
-
-        # Test case 3: Empty dictionary
-        data3 = {}
-        self.assertFalse(has_required_fields(data3))
     def test_valid_login(self):
     # Test case 1: Valid email and password
 
-    # with patch("module.get_user_model") as mock_get_user_model:
-    #     mock_user = MagicMock()
-    #     mock_user.password = "password123"
-    #     mock_get_user_model.return_value = mock_user
         email_attempt = "test@example.com"
         password_attempt = "password123"
-        assert valid_login(email_attempt, password_attempt) == True
+    #    self.assertEqual(account.valid_login(email_attempt,password_attempt),True)
 
     # Test case 2: Invalid email
-        assert valid_login("invalid@example.com", password_attempt) == False
+        self.assertEqual(account.valid_login("invalid@example.com", password_attempt),False)
 
     # Test case 3: Invalid password
-    # with patch("module.get_user_model") as mock_get_user_model:
-    #     mock_user = MagicMock()
-    #     mock_user.password = "password123"
-    #     mock_get_user_model.return_value = mock_user
-        assert valid_login(email_attempt, "wrongpassword") == False
+
+        self.assertEqual(account.valid_login(email_attempt, "wrongpassword"),False)
 
 #=======
 class TestDeleteAccount(TestCase):
